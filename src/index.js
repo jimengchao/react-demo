84a25092fae8dfd51843e2e3c04652e3d081709d
MuLuo <1097198549@qq.com>
2018-12-25 15:11:03 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDom from 'react-dom';
 
 import {Provider} from 'react-redux';
 import store from './redux/store';
-
+// 这是 dev- 1 注释
 import App from './router/index';
 // 2212312
 ReactDom.render(
     <Provider store={store}>
         <App />
-    </Provider>,
+    </Provider>, 
     document.getElementById('app')
 );
