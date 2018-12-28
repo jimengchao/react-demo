60b0bb3ffbd0e2d303cb5813ac4ddad683e22e5e
MuLuo <1097198549@qq.com>
2018-12-28 11:54:53 +0800

    2

--- src/redux/actions/userInfo.js
+++ src/redux/actions/userInfo.js
@@ -1,7 +1,7 @@
 export const GET_USER_INFO_REQUEST = "userInfo/GET_USER_INFO_REQUEST";
 export const GET_USER_INFO_SUCCESS = "userInfo/GET_USER_INFO_SUCCESS";
 export const GET_USER_INFO_FAIL = "userInfo/GET_USER_INFO_FAIL";
-
+// 21231
 function getUserInfoRequest() {
     return {
         type: GET_USER_INFO_REQUEST
